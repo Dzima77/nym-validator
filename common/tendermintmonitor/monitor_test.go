8ba72c08168d2ee4f1fee0e78e67f4bdfc20aa7c
Jedrzej Stuczynski <jedrzej.stuczynski@gmail.com>
2019-09-30 16:33:03 +0100

    Commented tests that wouldn't work externally

--- common/tendermintmonitor/monitor_test.go
+++ common/tendermintmonitor/monitor_test.go
@@ -17,226 +17,226 @@
 package monitor
 
 // TODO: MAKE THIS TEST WORK IN REMOTE ENVIRONMENTS
-
-import (
-	"io/ioutil"
-	"os"
-	"testing"
-	"time"
-
-	"github.com/nymtech/nym-validator/logger"
-	"github.com/nymtech/nym-validator/server/storage"
-	nymclient "github.com/nymtech/nym-validator/tendermint/client"
-	"github.com/stretchr/testify/assert"
-	cmn "github.com/tendermint/tendermint/libs/common"
-)
-
-const (
-	nodeAddress = "localhost:26657" // this particular chain currently is at block >10
-)
-
-func createValidMonitor() *Monitor {
-	tmpDir, err := ioutil.TempDir("", "monitor_test"+cmn.RandStr(6))
-	if err != nil {
-		panic(err)
-	}
-	defer os.RemoveAll(tmpDir)
-
-	disabledLog, err := logger.New("", "DEBUG", true)
-	if err != nil {
-		panic(err)
-	}
-
-	nymClient, err := nymclient.New([]string{nodeAddress}, disabledLog)
-	if err != nil {
-		panic(err)
-	}
-
-	store, err := storage.New("tmp_store", tmpDir)
-	if err != nil {
-		panic(err)
-	}
-
-	mon, err := New(disabledLog, nymClient, store, "foo")
-	if err != nil {
-		panic(err)
-	}
-	return mon
-}
-
-func TestNew(t *testing.T) {
-	validMon := createValidMonitor()
-
-	assert.NotNil(t, validMon)
-}
-
-func TestFinalizeFirstBlock(t *testing.T) {
-	// initial wait for starting blocks to appear
-	validMon := createValidMonitor()
-
-	for {
-		time.Sleep(time.Millisecond * 500)
-		if len(validMon.unprocessedBlocks) > 0 {
-			break
-		}
-	}
-
-	assert.Equal(t, int64(0), validMon.latestConsecutiveProcessed)
-
-	// pretest checks
-	_, ok := validMon.unprocessedBlocks[1]
-	assert.True(t, ok)
-	_, ok = validMon.processedBlocks[1]
-	assert.False(t, ok)
-
-	validMon.FinalizeHeight(1)
-	_, ok = validMon.unprocessedBlocks[1]
-	assert.False(t, ok)
-
-	// it's not added because if it's first, its deleted immediately
-	_, ok = validMon.processedBlocks[1]
-	assert.False(t, ok)
-
-	assert.Equal(t, int64(1), validMon.latestConsecutiveProcessed)
-
-}
-
-func TestFinalizeSingleBlock(t *testing.T) {
-	// initial wait for starting blocks to appear
-	validMon := createValidMonitor()
-
-	for {
-		time.Sleep(time.Millisecond * 500)
-		if len(validMon.unprocessedBlocks) > 2 {
-			break
-		}
-	}
-
-	assert.Equal(t, int64(0), validMon.latestConsecutiveProcessed)
-
-	// pretest checks
-	_, ok := validMon.unprocessedBlocks[1]
-	assert.True(t, ok)
-	_, ok = validMon.unprocessedBlocks[2]
-	assert.True(t, ok)
-	_, ok = validMon.unprocessedBlocks[3]
-	assert.True(t, ok)
-
-	_, ok = validMon.processedBlocks[1]
-	assert.False(t, ok)
-	_, ok = validMon.processedBlocks[2]
-	assert.False(t, ok)
-	_, ok = validMon.processedBlocks[3]
-	assert.False(t, ok)
-
-	validMon.FinalizeHeight(2)
-
-	_, ok = validMon.unprocessedBlocks[1]
-	assert.True(t, ok)
-	_, ok = validMon.unprocessedBlocks[2]
-	assert.False(t, ok)
-	_, ok = validMon.unprocessedBlocks[3]
-	assert.True(t, ok)
-
-	_, ok = validMon.processedBlocks[1]
-	assert.False(t, ok)
-	_, ok = validMon.processedBlocks[2]
-	assert.True(t, ok)
-	_, ok = validMon.processedBlocks[3]
-	assert.False(t, ok)
-
-	assert.Equal(t, int64(0), validMon.latestConsecutiveProcessed)
-
-}
-
-func TestFinalizeBlocksNotInOrder(t *testing.T) {
-	// initial wait for starting blocks to appear
-	validMon := createValidMonitor()
-
-	for {
-		time.Sleep(time.Millisecond * 500)
-		if len(validMon.unprocessedBlocks) > 2 {
-			break
-		}
-	}
-
-	assert.Equal(t, int64(0), validMon.latestConsecutiveProcessed)
-
-	// pretest checks
-	_, ok := validMon.unprocessedBlocks[1]
-	assert.True(t, ok)
-	_, ok = validMon.unprocessedBlocks[2]
-	assert.True(t, ok)
-	_, ok = validMon.unprocessedBlocks[3]
-	assert.True(t, ok)
-
-	_, ok = validMon.processedBlocks[1]
-	assert.False(t, ok)
-	_, ok = validMon.processedBlocks[2]
-	assert.False(t, ok)
-	_, ok = validMon.processedBlocks[3]
-	assert.False(t, ok)
-
-	validMon.FinalizeHeight(2)
-
-	_, ok = validMon.unprocessedBlocks[1]
-	assert.True(t, ok)
-	_, ok = validMon.unprocessedBlocks[2]
-	assert.False(t, ok)
-	_, ok = validMon.unprocessedBlocks[3]
-	assert.True(t, ok)
-
-	_, ok = validMon.processedBlocks[1]
-	assert.False(t, ok)
-	_, ok = validMon.processedBlocks[2]
-	assert.True(t, ok)
-	_, ok = validMon.processedBlocks[3]
-	assert.False(t, ok)
-
-	validMon.FinalizeHeight(1)
-
-	_, ok = validMon.unprocessedBlocks[1]
-	assert.False(t, ok)
-	_, ok = validMon.unprocessedBlocks[2]
-	assert.False(t, ok)
-	_, ok = validMon.unprocessedBlocks[3]
-	assert.True(t, ok)
-
-	_, ok = validMon.processedBlocks[1]
-	assert.False(t, ok)
-	_, ok = validMon.processedBlocks[2]
-	assert.False(t, ok)
-	_, ok = validMon.processedBlocks[3]
-	assert.False(t, ok)
-
-	assert.Equal(t, int64(2), validMon.latestConsecutiveProcessed)
-}
-
-func TestGetLowestUnprocessed(t *testing.T) {
-	// initial wait for starting blocks to appear
-	validMon := createValidMonitor()
-
-	for {
-		time.Sleep(time.Millisecond * 500)
-		if len(validMon.unprocessedBlocks) > 4 {
-			break
-		}
-	}
-
-	i, b := validMon.GetLowestUnprocessedBlock()
-
-	assert.Equal(t, int64(1), i)
-	assert.Equal(t, i, b.height)
-
-	i, b = validMon.GetLowestUnprocessedBlock()
-	assert.Equal(t, int64(2), i)
-	assert.Equal(t, i, b.height)
-
-	validMon.FinalizeHeight(1)
-	validMon.FinalizeHeight(2)
-	validMon.FinalizeHeight(3)
-
-	i, b = validMon.GetLowestUnprocessedBlock()
-	assert.Equal(t, int64(4), i)
-	assert.Equal(t, i, b.height)
-}
+//
+//import (
+//	"io/ioutil"
+//	"os"
+//	"testing"
+//	"time"
+//
+//	"github.com/nymtech/nym-validator/logger"
+//	"github.com/nymtech/nym-validator/server/storage"
+//	nymclient "github.com/nymtech/nym-validator/tendermint/client"
+//	"github.com/stretchr/testify/assert"
+//	cmn "github.com/tendermint/tendermint/libs/common"
+//)
+//
+//const (
+//	nodeAddress = "localhost:26657" // this particular chain currently is at block >10
+//)
+//
+//func createValidMonitor() *Monitor {
+//	tmpDir, err := ioutil.TempDir("", "monitor_test"+cmn.RandStr(6))
+//	if err != nil {
+//		panic(err)
+//	}
+//	defer os.RemoveAll(tmpDir)
+//
+//	disabledLog, err := logger.New("", "DEBUG", true)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	nymClient, err := nymclient.New([]string{nodeAddress}, disabledLog)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	store, err := storage.New("tmp_store", tmpDir)
+//	if err != nil {
+//		panic(err)
+//	}
+//
+//	mon, err := New(disabledLog, nymClient, store, "foo")
+//	if err != nil {
+//		panic(err)
+//	}
+//	return mon
+//}
+//
+//func TestNew(t *testing.T) {
+//	validMon := createValidMonitor()
+//
+//	assert.NotNil(t, validMon)
+//}
+//
+//func TestFinalizeFirstBlock(t *testing.T) {
+//	// initial wait for starting blocks to appear
+//	validMon := createValidMonitor()
+//
+//	for {
+//		time.Sleep(time.Millisecond * 500)
+//		if len(validMon.unprocessedBlocks) > 0 {
+//			break
+//		}
+//	}
+//
+//	assert.Equal(t, int64(0), validMon.latestConsecutiveProcessed)
+//
+//	// pretest checks
+//	_, ok := validMon.unprocessedBlocks[1]
+//	assert.True(t, ok)
+//	_, ok = validMon.processedBlocks[1]
+//	assert.False(t, ok)
+//
+//	validMon.FinalizeHeight(1)
+//	_, ok = validMon.unprocessedBlocks[1]
+//	assert.False(t, ok)
+//
+//	// it's not added because if it's first, its deleted immediately
+//	_, ok = validMon.processedBlocks[1]
+//	assert.False(t, ok)
+//
+//	assert.Equal(t, int64(1), validMon.latestConsecutiveProcessed)
+//
+//}
+//
+//func TestFinalizeSingleBlock(t *testing.T) {
+//	// initial wait for starting blocks to appear
+//	validMon := createValidMonitor()
+//
+//	for {
+//		time.Sleep(time.Millisecond * 500)
+//		if len(validMon.unprocessedBlocks) > 2 {
+//			break
+//		}
+//	}
+//
+//	assert.Equal(t, int64(0), validMon.latestConsecutiveProcessed)
+//
+//	// pretest checks
+//	_, ok := validMon.unprocessedBlocks[1]
+//	assert.True(t, ok)
+//	_, ok = validMon.unprocessedBlocks[2]
+//	assert.True(t, ok)
+//	_, ok = validMon.unprocessedBlocks[3]
+//	assert.True(t, ok)
+//
+//	_, ok = validMon.processedBlocks[1]
+//	assert.False(t, ok)
+//	_, ok = validMon.processedBlocks[2]
+//	assert.False(t, ok)
+//	_, ok = validMon.processedBlocks[3]
+//	assert.False(t, ok)
+//
+//	validMon.FinalizeHeight(2)
+//
+//	_, ok = validMon.unprocessedBlocks[1]
+//	assert.True(t, ok)
+//	_, ok = validMon.unprocessedBlocks[2]
+//	assert.False(t, ok)
+//	_, ok = validMon.unprocessedBlocks[3]
+//	assert.True(t, ok)
+//
+//	_, ok = validMon.processedBlocks[1]
+//	assert.False(t, ok)
+//	_, ok = validMon.processedBlocks[2]
+//	assert.True(t, ok)
+//	_, ok = validMon.processedBlocks[3]
+//	assert.False(t, ok)
+//
+//	assert.Equal(t, int64(0), validMon.latestConsecutiveProcessed)
+//
+//}
+//
+//func TestFinalizeBlocksNotInOrder(t *testing.T) {
+//	// initial wait for starting blocks to appear
+//	validMon := createValidMonitor()
+//
+//	for {
+//		time.Sleep(time.Millisecond * 500)
+//		if len(validMon.unprocessedBlocks) > 2 {
+//			break
+//		}
+//	}
+//
+//	assert.Equal(t, int64(0), validMon.latestConsecutiveProcessed)
+//
+//	// pretest checks
+//	_, ok := validMon.unprocessedBlocks[1]
+//	assert.True(t, ok)
+//	_, ok = validMon.unprocessedBlocks[2]
+//	assert.True(t, ok)
+//	_, ok = validMon.unprocessedBlocks[3]
+//	assert.True(t, ok)
+//
+//	_, ok = validMon.processedBlocks[1]
+//	assert.False(t, ok)
+//	_, ok = validMon.processedBlocks[2]
+//	assert.False(t, ok)
+//	_, ok = validMon.processedBlocks[3]
+//	assert.False(t, ok)
+//
+//	validMon.FinalizeHeight(2)
+//
+//	_, ok = validMon.unprocessedBlocks[1]
+//	assert.True(t, ok)
+//	_, ok = validMon.unprocessedBlocks[2]
+//	assert.False(t, ok)
+//	_, ok = validMon.unprocessedBlocks[3]
+//	assert.True(t, ok)
+//
+//	_, ok = validMon.processedBlocks[1]
+//	assert.False(t, ok)
+//	_, ok = validMon.processedBlocks[2]
+//	assert.True(t, ok)
+//	_, ok = validMon.processedBlocks[3]
+//	assert.False(t, ok)
+//
+//	validMon.FinalizeHeight(1)
+//
+//	_, ok = validMon.unprocessedBlocks[1]
+//	assert.False(t, ok)
+//	_, ok = validMon.unprocessedBlocks[2]
+//	assert.False(t, ok)
+//	_, ok = validMon.unprocessedBlocks[3]
+//	assert.True(t, ok)
+//
+//	_, ok = validMon.processedBlocks[1]
+//	assert.False(t, ok)
+//	_, ok = validMon.processedBlocks[2]
+//	assert.False(t, ok)
+//	_, ok = validMon.processedBlocks[3]
+//	assert.False(t, ok)
+//
+//	assert.Equal(t, int64(2), validMon.latestConsecutiveProcessed)
+//}
+//
+//func TestGetLowestUnprocessed(t *testing.T) {
+//	// initial wait for starting blocks to appear
+//	validMon := createValidMonitor()
+//
+//	for {
+//		time.Sleep(time.Millisecond * 500)
+//		if len(validMon.unprocessedBlocks) > 4 {
+//			break
+//		}
+//	}
+//
+//	i, b := validMon.GetLowestUnprocessedBlock()
+//
+//	assert.Equal(t, int64(1), i)
+//	assert.Equal(t, i, b.height)
+//
+//	i, b = validMon.GetLowestUnprocessedBlock()
+//	assert.Equal(t, int64(2), i)
+//	assert.Equal(t, i, b.height)
+//
+//	validMon.FinalizeHeight(1)
+//	validMon.FinalizeHeight(2)
+//	validMon.FinalizeHeight(3)
+//
+//	i, b = validMon.GetLowestUnprocessedBlock()
+//	assert.Equal(t, int64(4), i)
+//	assert.Equal(t, i, b.height)
+//}
